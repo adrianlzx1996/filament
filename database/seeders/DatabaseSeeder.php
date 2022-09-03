56d28f878693e40c1209f5ef39d919faf0ad9dce
Adrian <leongadrian36@gmail.com>
2022-09-03 16:40:45 +0800

    project migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,24 +1,25 @@
 <?php
 
-namespace Database\Seeders;
+    namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
+    use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
+    class DatabaseSeeder extends Seeder
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        /**
+         * Seed the application's database.
+         *
+         * @return void
+         */
+        public function run ()
+        {
+            $this->call(RolesSeeder::class);
+            $this->call(AdminUserSeeder::class);
+            $this->call(ProductSeeder::class);
+            $this->call(VoucherSeeder::class);
+            $this->call(UserSeeder::class);
+            $this->call(PaymentSeeder::class);
+            $this->call(CategorySeeder::class);
+            $this->call(TagsSeeder::class);
+        }
     }
-}
