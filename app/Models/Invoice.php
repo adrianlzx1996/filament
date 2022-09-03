173e8d846b7ff44c1681bb2f11ba5528ebbe3c84
Adrian <leongadrian36@gmail.com>
2022-09-03 16:40:50 +0800

    project models

---
+++ app/Models/Invoice.php
@@ -0,0 +1,24 @@
+<?php
+
+    namespace App\Models;
+
+    use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\Relations\BelongsTo;
+    use Illuminate\Database\Eloquent\Relations\HasMany;
+
+    class Invoice extends Model
+    {
+        protected $fillable = [ 'user_id', 'invoice_date', 'invoice_number' ];
+
+        public function user ()
+        : BelongsTo
+        {
+            return $this->belongsTo(User::class);
+        }
+
+        public function invoiceItems ()
+        : HasMany
+        {
+            return $this->hasMany(InvoiceItem::class);
+        }
+    }
