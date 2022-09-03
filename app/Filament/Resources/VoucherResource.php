288392586b6a7f2a7522367cdd29e879aef8ec78
Adrian <leongadrian36@gmail.com>
2022-09-03 17:42:03 +0800

    setup voucher form + table

--- app/Filament/Resources/VoucherResource.php
+++ app/Filament/Resources/VoucherResource.php
@@ -1,62 +1,73 @@
 <?php
 
-namespace App\Filament\Resources;
+    namespace App\Filament\Resources;
 
-use App\Filament\Resources\VoucherResource\Pages;
-use App\Filament\Resources\VoucherResource\RelationManagers;
-use App\Models\Voucher;
-use Filament\Forms;
-use Filament\Resources\Form;
-use Filament\Resources\Resource;
-use Filament\Resources\Table;
-use Filament\Tables;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+    use App\Filament\Resources\VoucherResource\Pages;
+    use App\Filament\Resources\VoucherResource\RelationManagers;
+    use App\Models\Voucher;
+    use Filament\Forms\Components\Select;
+    use Filament\Forms\Components\TextInput;
+    use Filament\Resources\Form;
+    use Filament\Resources\Resource;
+    use Filament\Resources\Table;
+    use Filament\Tables;
 
-class VoucherResource extends Resource
-{
-    protected static ?string $model = Voucher::class;
-
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
-
-    public static function form(Form $form): Form
+    class VoucherResource extends Resource
     {
-        return $form
-            ->schema([
+        protected static ?string $model = Voucher::class;
+
+        protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+        public static function form ( Form $form )
+        : Form {
+            return $form
+                ->schema([
+                             TextInput::make('code')
+                                      ->required(),
+                             TextInput::make('discount_percent')
+                                      ->required()
+                                      ->rule('numeric'),
+                             Select::make('product_id')
+                                   ->relationship('product', 'name'),
+                         ])
+            ;
+        }
+
+        public static function table ( Table $table )
+        : Table {
+            return $table
+                ->columns([
+                              Tables\Columns\TextColumn::make('code'),
+                              Tables\Columns\TextColumn::make('discount_percent'),
+                              Tables\Columns\TextColumn::make('product.name'),
+                          ])
+                ->filters([
+                              //
+                          ])
+                ->actions([
+                              Tables\Actions\EditAction::make(),
+                          ])
+                ->bulkActions([
+                                  Tables\Actions\DeleteBulkAction::make(),
+                              ])
+            ;
+        }
+
+        public static function getRelations ()
+        : array
+        {
+            return [
                 //
-            ]);
-    }
+            ];
+        }
 
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                //
-            ])
-            ->filters([
-                //
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\DeleteBulkAction::make(),
-            ]);
+        public static function getPages ()
+        : array
+        {
+            return [
+                'index'  => Pages\ListVouchers::route('/'),
+                'create' => Pages\CreateVoucher::route('/create'),
+                'edit'   => Pages\EditVoucher::route('/{record}/edit'),
+            ];
+        }
     }
-    
-    public static function getRelations(): array
-    {
-        return [
-            //
-        ];
-    }
-    
-    public static function getPages(): array
-    {
-        return [
-            'index' => Pages\ListVouchers::route('/'),
-            'create' => Pages\CreateVoucher::route('/create'),
-            'edit' => Pages\EditVoucher::route('/{record}/edit'),
-        ];
-    }    
-}
